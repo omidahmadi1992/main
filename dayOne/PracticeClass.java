649bad41a05d1d886e3b70a9469c252cb9424f71
omidahmadi1992 <mr_omidahmadi@yahoo.com>
2022-06-21 22:03:42 -0700

    message

--- dayOne/PracticeClass.java
+++ dayOne/PracticeClass.java
@@ -6,6 +6,20 @@ public static void main (String []args) {
 
 System.out.println("Hello");
 
+int a = 1;
+int b = 24;
+int total = a+b;
+System.out.println(total);
 
+
+
+if (total == 25) {
+	System.out.println("you got it");
+	
+} else {
+		System.out.println("the anwser is not correct");
+	
 }
 }
+}
+
